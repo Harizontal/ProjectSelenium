43b1254fc62688076e5c8b812f127ebd80cf5578
Alexey Machihine <alesha.machihine2005@mail.ru>
2024-08-12 15:07:59 +0300

    Задания по навигации Selenium

---
+++ SeleniumInitialize_Tests/NavigationTo/NavigationTo1.md
@@ -0,0 +1,14 @@
+# 1
+Страница: https://ib.psbank.ru/;
+Тип локатора: XPath;
+Значение локатора: "//h2[contains(text(), 'Финансовые продукты')]";
+
+# 2
+Страница: https://ib.psbank.ru/store/products/consumer-loan;
+Тип локатора: XPath;
+Значение локатора: "//h2[contains(text(), 'Как получить кредит')]";
+
+# 3 
+Страница: https://ib.psbank.ru/store/products/investmentsbrokerage/;
+Тип локатора: XPath;
+Значение локатора: "//h2[contains(text(), 'Заключить договор через Госуслуги')]";
---
+++ SeleniumInitialize_Tests/NavigationTo/NavigationTo2.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumInitialize_Builder;
+
+namespace SeleniumInitialize_Tests.NavigationTo
+{
+    public class NavigationTo2 : TestBaseSetUp
+    {
+        [Test(Description = "Проверка полноценной загрузки сайта")]
+        public void HomePageLoadsSuccessfully()
+        {
+            IWebDriver driver = Builder.WithURL("https://ib.psbank.ru/").Build();
+            var browser = Builder.GetBrowser();
+            browser.WaitForPageLoad(By.XPath("//h2[contains(text(), 'Финансовые продукты')]"));
+            Assert.That(driver.FindElements(By.XPath("//h2[contains(text(), 'Финансовые продукты')]")).SingleOrDefault(), Is.Not.Null);
+        }
+    }
+}
